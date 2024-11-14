e600bdf0f3cf1254b14887cf85013049ee6bf4cc
elsahv <elsahvy@gmail.com>
2024-11-13 17:20:32 -0800

    save

--- src/components/SignedOutContent.jsx
+++ src/components/SignedOutContent.jsx
@@ -6,12 +6,12 @@ const SignedOutContent = () => {
     <>
       {/* TEST V */}
       <>
-        <div id="main-container" className="">
+        <div id="main-container" className="h-[810px]">
           <div className=" bg-munsell border-2 border-black ">
             <div id="grid" className="grid grid-cols-2 ">
               <div id="sq1">
                 {" "}
-                <img src={JtLogo} alt="" className="2xl:h-[650px] 2xl:w-full" />
+                <img src={JtLogo} alt="" className="2xl:w-full" />
               </div>
 
               <div
@@ -20,23 +20,11 @@ const SignedOutContent = () => {
               >
                 <div id="content-container" className="m-5 px-5">
                   <h2 className="text-4xl  ">Need to Login!</h2>
-                  <p className=" mt-1 text-xl  ">
-                    Welcome to the Running Quail, a Morongo Basin Community.
-                  </p>
-                  <p className="text-lg   pt-1 ">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Animi odit quia quos voluptate provident facilis, alias
-                    deleniti, quo libero nemo vitae beatae, placeat repudiandae
-                    aut. Consequuntur, doloremque. Praesentium, debitis odio?
-                  </p>
+
                   <p className="text-lg   pt-1">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Animi odit quia quos voluptate provident facilis, alias
-                    deleniti, quo libero nemo vitae beatae, placeat repudiandae
-                    aut. Consequuntur, doloremque. Praesentium, debitis
-                    odio?Lorem ipsum dolor sit amet consectetur, adipisicing
-                    elit. Animi odit quia quos voluptate provident facilis,
-                    alias deleniti, quo libero nemo vitae beata
+                    Animi odit quia quos voluptate provident facilis, alias del
+                    ovident facilis, alias deleniti, quo libero nemo vitae beata
                   </p>
 
                   <div id="form-container" className="mt-1 pt-3 pl-1">
--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -1,5 +1,4 @@
-import TestImg from "../assets/jtVertical.jpg";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 
 import SignedInContent from "../components/SignedInContent";
 import SignedOutContent from "../components/SignedOutContent";
